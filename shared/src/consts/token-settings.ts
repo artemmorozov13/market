9436a6940ae109243a2d73c8a6650dfd00300911
Артем Морозов <artemmorozov@Artems-MacBook-Pro.local>
2025-07-02 10:46:48 +0500

    mvp ready

--- shared/src/consts/token-settings.ts
+++ shared/src/consts/token-settings.ts
@@ -1,13 +1,11 @@
-import { CookieAttributes } from "js-cookie";
-
-export const accessCookiesOptions: CookieAttributes = {
+export const accessCookiesOptions = {
   expires: new Date(Date.now() + 30 * 60 * 1000),
   // secure: true,
   // httpOnly: true,
   // sameSite: 'strict'
 };
 
-export const refreshCookiesOptions: CookieAttributes = {
+export const refreshCookiesOptions = {
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   // secure: false,
   // httpOnly: true,
