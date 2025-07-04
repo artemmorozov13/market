8f0d8e68c431159f0b99dcc13db0719305058a99
Артем Морозов <artemmorozov@Artems-MacBook-Pro.local>
2025-07-04 11:30:56 +0500

    add pickup point

--- apps/admin/src/shared/api/instance.ts
+++ apps/admin/src/shared/api/instance.ts
@@ -3,12 +3,10 @@ import Cookies from "js-cookie";
 import { ACCESS_TOKEN } from "../lib/consts/consts";
 import { requestTokenMiddleware } from "@shared/lib/middlewares/requestAccessTokenMiddleWare";
 
-const accessToken = Cookies.get(ACCESS_TOKEN)
-
 const API = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_HOST,
     headers: {
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${Cookies.get(ACCESS_TOKEN)}`
     }
 })
 
