07c3cf3010cfdeb8a41eea022ec376c99da3cb1b
Артем Морозов <artemmorozov@Artems-MacBook-Pro.local>
2025-07-07 00:22:13 +0500

    fix token error

--- apps/admin/src/shared/api/instance.ts
+++ apps/admin/src/shared/api/instance.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
-import Cookies from "js-cookie";
-import { ACCESS_TOKEN } from "../lib/consts/consts";
 import { requestTokenMiddleware } from "@shared/lib/middlewares/requestAccessTokenMiddleWare";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_HOST,
-    headers: {
-        Authorization: `Bearer ${Cookies.get(ACCESS_TOKEN)}`
-    }
 })
 
 API.interceptors.request.use(requestTokenMiddleware)
--- apps/app/src/shared/api/API.ts
+++ apps/app/src/shared/api/API.ts
@@ -1,13 +1,8 @@
 import axios from "axios"
-import Cookies from "js-cookie"
-import { ACCESS_TOKEN } from "../consts/applicationConsts"
 import { requestTokenMidleware } from "../middlware/requestRefreshTokenMiddleware";
 
 export const API = axios.create({
     baseURL: process.env.VITE_BACKEND_HOST,
-    headers: {
-        authorization: `Bearer ${Cookies.get(ACCESS_TOKEN)}`,
-    }
 })
 
 API.interceptors.request.use(requestTokenMidleware);
