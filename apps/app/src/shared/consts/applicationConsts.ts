e2df680f6baa7e0e0a408f20f0d21f732569ca1d
Артем Морозов <artemmorozov@Artems-MacBook-Pro.local>
2025-08-11 01:49:06 +0500

    prod

--- apps/app/src/shared/consts/applicationConsts.ts
+++ apps/app/src/shared/consts/applicationConsts.ts
@@ -1,5 +1,5 @@
-export const ACCESS_TOKEN = 'access-token';
-export const REFRESH_TOKEN = 'refresh-token';
+export const ACCESS_TOKEN = 'app-access-token';
+export const REFRESH_TOKEN = 'app-refresh-token';
 
 export const MINIMUM_QUANTITY_TO_BE_IN_BASKET = 1;
 export const LOCALSTORAGE_STOREID_KEY = 'storeid_key';
