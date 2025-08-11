2c0e238e314c05847c70c74ca906fa2de09fc7f4
Артем Морозов <artemmorozov@Artems-MacBook-Pro.local>
2025-08-11 21:28:10 +0500

    save

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -29,8 +29,8 @@ async function bootstrap() {
     prefix: '/static/',
   });
 
-  console.log(`Server started on port: ${process.env.PORT}`)
+  console.log(`Server started on port: ${process.env.PORT || 8000}`)
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 8000, '0.0.0.0');
 }
 bootstrap();
